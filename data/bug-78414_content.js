1a9b52e882a45b15bc6f1b9b0e1587486cac6c1e
Gregorio Litenstein <g.litenstein@gmail.com>
2014-08-12 13:30:01 -0400

    Removed left-over junk and debug messages.

--- data/bug-78414_content.js
+++ data/bug-78414_content.js
@@ -8,19 +8,12 @@ self.port.on("findPlugins", function(workerID) { // Look for objects to modify.
     var stealFocusDelay = self.options.stealFocusDelay;
     var shiftAllowFocus = self.options.shiftAllowFocus;
     var embeds = (document.querySelectorAll(cssSelectors) || []);
-    // embeds && console.warn("DEBUG:	queryselectorall: "+embeds.length);
+
     for (let i = 0; i < (embeds.length || 0); i++) {
-        console.warn("DEBUG:	Embed: " + embeds[i]);
-        if (embeds[i].hasAttribute("onClick")) { // If we've already got an onClick, make sure to save it in case the user wants to disable this feature.
-            // console.warn("DEBUG:	Item already has onClick property, copying it for reference later.");
-            embeds[i].setAttribute("onClickOriginal", embeds[i].getAttribute("onClick"));
-        }
-        embeds[i].setAttribute("onClick", "this.blur()");
 
         var listener = function(evt) {
                 if ((true != shiftAllowFocus || true != evt.shiftKey) && (1 == evt.which)) {
                     window.setTimeout(function() {
-                        alert('This is a timeout.');
                         evt.target.blur()
                     }, stealFocusDelay)
                 }
@@ -29,11 +22,6 @@ self.port.on("findPlugins", function(workerID) { // Look for objects to modify.
         clickListeners[embeds[i].id] = listener; // Store a reference to the new listener for eventual removal.
     }
     if (embeds.length < 1) {
-
-        // console.warn ("DEBUG:	Worker ID: "+workerID.number+" found no objects to work on; reporting to addon.");
-        self.port.emit("noEmbeds", workerID);
-
-
         self.port.emit("noEmbeds", workerID); // Being useless, report back for "handling."
     }
 
@@ -43,6 +31,5 @@ self.port.on("removeMods", function removeMods(workerID) {
     for (let i of Object.keys(clickListeners)) {
         document.getElementById(i).removeEventListener('click', clickListeners[i]);
     }
-
     self.port.emit("noEmbeds", workerID); // Commit Seppuku.
 });
