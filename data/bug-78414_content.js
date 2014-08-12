50851b76dd42206bd28a0e56c8dec3fbfa0a8e73
Gregorio Litenstein <g.litenstein@gmail.com>
2014-08-12 13:19:44 -0400

    Indentation
    Blame JS Beautifier...

--- data/bug-78414_content.js
+++ data/bug-78414_content.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-	var clickListeners = {};
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+var clickListeners = {};
 
 self.port.on("findPlugins", function(workerID) { // Look for objects to modify.
     var cssSelectors = self.options.cssSelectors;
