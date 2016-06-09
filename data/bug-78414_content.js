152204f132f2ec6fe4eb5a3f30d83b8b568a27de
Gregorio Litenstein <g.litenstein@gmail.com>
2016-06-08 22:53:39 -0400

    The bug-78414 workaround is now more robust
    Added support to retry crawling a page where no embeds were found.

--- data/bug-78414_content.js
+++ data/bug-78414_content.js
@@ -2,8 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 var clickListeners = {};
+var timesCrawled = 0;
+var pluginCrawlLimit = self.options.pluginCrawlLimit;
+console.warn("Setting timesCrawled to 0!");
 
-self.port.on("findPlugins", function(workerID) { // Look for objects to modify.
+var checkAgain = function () {
+	console.warn("A DOM node changed, should we rescan for embeds?");
+	if (timesCrawled <= pluginCrawlLimit) {
+		timesCrawled++;
+		console.warn("YES! We should keep searching!");
+		self.port.emit("noEmbeds", { "ID": workerID, "checkAgain": true } ); // Fail me ONE more time...
+	}
+	else {
+		console.warn("NO! We should not keep searching!");
+		self.port.emit("noEmbeds", { "ID": workerID, "checkAgain": false } );
+	}
+}
+
+function checkPlugins (workerID) {
     var cssSelectors = self.options.cssSelectors;
     var stealFocusDelay = self.options.stealFocusDelay;
     var shiftAllowFocus = self.options.shiftAllowFocus;
@@ -14,22 +30,47 @@ self.port.on("findPlugins", function(workerID) { // Look for objects to modify.
         var listener = function(evt) {
                 if ((true != shiftAllowFocus || true != evt.shiftKey) && (1 == evt.which)) {
                     window.setTimeout(function() {
+                    	window.alert('THE EVENT IS WORKING');
                         evt.target.blur()
                     }, stealFocusDelay)
                 }
             } // 		Capture only left-clicks, and check whether we want to ignore shift+click.
+            console.warn("Currently looking at embed: "+embeds[i].getAttribute("id"));
+            if (!embeds[i].hasAttribute('KeybinderListenerAdded')) {
             embeds[i].addEventListener('click', listener);
+            embeds[i].setAttribute('KeybinderListenerAdded',true);
+            }
+            else { console.log(embeds[i].getAttribute("id")+" already has a listener, skipping!"); }
         clickListeners[embeds[i].id] = listener; // Store a reference to the new listener for eventual removal.
-    }
+    } 
     if (embeds.length < 1) {
-        self.port.emit("noEmbeds", workerID); // Being useless, report back for "handling."
-    }
+    	console.warn("[CONTENT SCRIPT] Did not find any embeds!");
+        console.warn("[CONTENT SCRIPT] workerID number is...? "+workerID);
+        let body = document.getElementsByTagName("BODY")[0];
+		if (!(body.hasAttribute("KeybinderListenerAdded"))) {
+		body.addEventListener("DOMNodeInserted", checkAgain);
+    	body.setAttribute("KeybinderListenerAdded",true);
+    	}
+	}
+}
 
+self.port.on("findPlugins", function findPlugins(workerInfo) {// Look for objects to modify.
+console.warn("[CONTENT SCRIPT] Checking plugins, try n°: "+timesCrawled);
+workerID = workerInfo["ID"].toString();
+checkPlugins(workerID);
+});
+
+self.port.on("removeDOMListener", function removeDOMListener() {
+    let body = document.getElementsByTagName("BODY")[0];
+	body.removeEventListener("DOMNodeInserted", checkAgain);
+    body.removeAttribute("KeybinderListenerAdded");
+    self.port.emit("disposeOfMinion", { "workerID": workerID }); // Useless worker will need to be... "handled."
 });
 
 self.port.on("removeMods", function removeMods(workerID) {
     for (let i of Object.keys(clickListeners)) {
         document.getElementById(i).removeEventListener('click', clickListeners[i]);
+        document.getElementById(i).removeAttribute('KeybinderListenerAdded');
     }
-    self.port.emit("noEmbeds", workerID); // Commit Seppuku.
+		self.port.emit("noEmbeds", { "ID": workerID, "checkAgain": false } );
 });
